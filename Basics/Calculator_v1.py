1df025df2de98fe7995b3feecd11c59c3627cd93
Almond0906 <roshenreji09@gmail.com>
2025-09-28 16:56:15 +0530

    Added Permutation and Combination functions

--- Basics/Calculator_v1.py
+++ Basics/Calculator_v1.py
@@ -1,14 +1,15 @@
 import math
 def main():
-  num_1 = float(input("Enter the first number"))
-  char_1 = input("Enter the operator")
+  num_1 = float(input("Enter the first number: "))
+  char_1 = input("Enter the operator: ")
   if char_1 in ("+","-","X","x","*","/","÷","^"):
     arith(num_1,char_1)
   elif char_1 in ("sqrt","Sqrt","!","cbrt","Cbrt","√","fact"):
     non_arith(num_1,char_1)
-
+  elif char_1 in ("Combination","C","c","Comb","p","Permutation","permute","P"):
+    selection(num_1,char_1)
 def arith(num_1,char_1):## used the same Variables for easier recognition 
-  num_2 = float(input("Enter the Second Number"))
+  num_2 = float(input("Enter the Second Number: "))
   match char_1:
     case "+":
       print(f"{num_1} + {num_2} = {num_1 + num_2}")
@@ -35,5 +36,15 @@ def non_arith(num_1,char_1):
       else:
         print(f"The Cube root of {num_1} is {math.cbrt(num_1)}")
     case "fact"|"!":
-      print(f"The Factorial of {num_1} is {math.factorial(int(num_1))}")
+      if num_1<0:
+        print("Invalid Entry")
+      print(f"The Factorial of {int(num_1)} is {math.factorial(int(num_1))}")
+def selection(num_1,char_1):
+  num_2=int(input("Enter the number of items to be selected: "))
+  from sympy import symbols,pretty
+  match char_1:
+    case "P"|"Permutation"|"p"|"permute":
+      print(f"{int(num_1)} P {int(num_2)} = {math.perm(int(num_1),int(num_2))}")
+    case "C"|"c"|"Combination"|"comb":
+      print(f"{int(num_1)} C {int(num_2)} = {math.comb(int(num_1),int(num_2))}")
 main()
