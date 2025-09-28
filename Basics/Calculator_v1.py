fa371bbb30ff755e3cf653ff67be8c62e240c69c
Almond0906 <roshenreji09@gmail.com>
2025-09-28 15:31:28 +0530

    Added Basics folder and simple calculator program

---
+++ Basics/Calculator_v1.py
@@ -0,0 +1,39 @@
+import math
+def main():
+  num_1 = float(input("Enter the first number"))
+  char_1 = input("Enter the operator")
+  if char_1 in ("+","-","X","x","*","/","÷","^"):
+    arith(num_1,char_1)
+  elif char_1 in ("sqrt","Sqrt","!","cbrt","Cbrt","√","fact"):
+    non_arith(num_1,char_1)
+
+def arith(num_1,char_1):## used the same Variables for easier recognition 
+  num_2 = float(input("Enter the Second Number"))
+  match char_1:
+    case "+":
+      print(f"{num_1} + {num_2} = {num_1 + num_2}")
+    case "-":
+      print(f"{num_1} - {num_2} = {num_1 - num_2}")
+    case "X"|"x"|"*":
+      print (f"{num_1 } x {num_2} = {num_1 * num_2}")
+    case "/"|"÷":
+      print(f"{num_1} ÷ {num_2} = {num_1 / num_2}")
+    case "^":
+      from sympy import symbols,pretty
+      base = symbols(str(int(num_1)))
+      print(f"{pretty(base ** int(num_2))} = {int(num_1) ** int(num_2)}")
+def non_arith(num_1,char_1):
+  match char_1:
+    case "sqrt"|"Sqrt"|"√":
+      if num_1 < 0:
+        print("Invalid Enteries")
+      else:
+          print(f"The Square root of {num_1} is {math.sqrt(num_1)}")
+    case "cbrt"|"Cbrt":
+      if num_1 < 0:
+        print("Invalid Enteries")
+      else:
+        print(f"The Cube root of {num_1} is {math.cbrt(num_1)}")
+    case "fact"|"!":
+      print(f"The Factorial of {num_1} is {math.factorial(int(num_1))}")
+main()
